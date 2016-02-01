7458fb96e697cdc1e6753989141098ba9345d9ff
linn <linn@firewinggames.com>
2016-02-01 14:33:46 +0800

    Add server

---
+++ TriSuperSocketWithUnity/server/server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using SuperSocket.SocketBase;
+
+namespace server
+{
+    class MainClass
+    {
+        const int port = 1254;
+
+        public static void Main(string[] args)
+        {
+            AppServer appServer = new AppServer();
+
+            appServer.NewRequestReceived += AppServer_NewRequestReceived;
+            appServer.NewSessionConnected += AppServer_NewSessionConnected;
+            appServer.SessionClosed += AppServer_SessionClosed;
+
+            //Setup
+            if (!appServer.Setup(port))
+            {
+                Console.WriteLine("Failded to Setup!");
+                Console.ReadKey();
+                return;
+            }
+
+            //Start
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to Start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            //Stop
+            appServer.Stop();
+        }
+
+        static void AppServer_SessionClosed (AppSession session, CloseReason value)
+        {
+            Console.WriteLine("AppServer_SessionClosed " + session.SessionID + " " + value);
+        }
+
+        static void AppServer_NewSessionConnected (AppSession session)
+        {
+            Console.WriteLine("AppServer_NewSessionConnected " + session.SessionID);
+        }
+
+        static void AppServer_NewRequestReceived (AppSession session, SuperSocket.SocketBase.Protocol.StringRequestInfo requestInfo)
+        {
+            Console.WriteLine("AppServer_NewRequestReceived " + session.SessionID + " " + requestInfo);
+        }
+    }
+}
