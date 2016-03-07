ea8ff6e794e7975e1d7f39221a5da73aa3162ba7
linn <linn@firewinggames.com>
2016-03-07 23:14:52 +0800

    update

---
+++ Java/Webcome.java
@@ -0,0 +1,15 @@
+public class Webcome
+{
+    public static void main(String[] args)
+    {
+        String[] greeting = new String[3];
+        greeting[0] = "Webcom to Core Java";
+        greeting[1] = "by CH";
+        greeting[2] = "and GC";
+
+        for (String g : greeting)
+        {
+            System.out.println(g);
+        }
+    }
+}
