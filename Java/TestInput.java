6f5e7aa5065927a1ba8ca73f3e26cb0e055ad6c3
linn <linn@firewinggames.com>
2016-03-08 11:54:26 +0800

    update

---
+++ Java/TestInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TestInput
+{
+	public static void main(String[] argv)
+	{
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		System.out.println(t);
+	}
+}
