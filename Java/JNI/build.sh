42ebf2f82295c132faef8fa0224c81520adf51df
linn <linn@firewinggames.com>
2016-03-14 19:26:58 +0800

    update

--- Java/JNI/build.sh
+++ Java/JNI/build.sh
@@ -1,5 +1,9 @@
 export JDK=/System/Library/Frameworks/JavaVM.framework/Versions/A
-gcc -dynamiclib -I $JDK/Headers -shared -o libHelloNative.jnilib HelloNative.c
+gcc -dynamiclib \
+	-I $JDK/Headers \
+	-shared \
+	-o libHelloNative.jnilib \
+	HelloNative.c
 javac HelloNative.java
 javac HelloNativeTest.java
 java -Djava.library.path=. HelloNativeTest
