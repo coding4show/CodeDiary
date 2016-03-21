cf8b4097869776f3243d75b8fa86ba7b75faa417
linn <linn@firewinggames.com>
2016-03-21 16:41:13 +0800

    Unity图片检查工具

---
+++ UnityTips/TextureImportTools.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+/// <summary>
+/// 检查Unity工程中, 没能载入成功的图片
+/// </summary>
+public class TextureImportTools : EditorWindow {
+	[MenuItem("Tools/Asset/TextureImportTool")]
+	public static void ShowView(){
+		EditorWindow.GetWindow<TextureImportTools>();
+	}
+	
+	Texture2D texture = null;
+	void OnGUI(){
+		if (GUILayout.Button("Check")){
+			string[] imageSuffixs = new string[]{"png", "jpg", "tga", "psd"};
+			
+			string[] guids = AssetDatabase.FindAssets("t:DefaultAsset");		
+			foreach (var guid in guids){
+				string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+				foreach (var suffix in imageSuffixs){
+					if (assetPath.EndsWith(suffix)){
+						Debug.LogError("Error : " + assetPath);
+						AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
+						break;
+					}
+				}
+			}
+			
+			Debug.Log("Check Over");
+		}
+	}
+}
