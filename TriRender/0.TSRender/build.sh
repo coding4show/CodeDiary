119a96c65e767a3e8c50015cdabfa92cef525303
linn <linn@firewinggames.com>
2016-04-13 11:30:38 +0800

    update

--- TriRender/0.TSRender/build.sh
+++ TriRender/0.TSRender/build.sh
@@ -1,3 +1,4 @@
-tsc --target ES5 render/Render.ts
-tsc --target ES5 objectparser/ObjectParser.ts
-tsc --target ES5 book/Book.ts
+#tsc --target ES5 render/Render.ts
+#tsc --target ES5 objectparser/ObjectParser.ts
+#tsc --target ES5 book/Book.ts
+tsc -p .
